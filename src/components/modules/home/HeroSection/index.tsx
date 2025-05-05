380744dbb9b8b137061449826fe314a80ab4ad7c
MarGoN <monondcosta@gmail.com>
2025-05-06 04:29:26 +0600

    Modify: modified Hero section's button

--- src/components/modules/home/HeroSection/index.tsx
+++ src/components/modules/home/HeroSection/index.tsx
@@ -1,8 +1,8 @@
 "use client";
 
-import CustomButton from "@/components/shared/CustomButton";
 import styles from "./HeroSection.module.css";
 import { useRouter } from "next/navigation";
+import { Button } from "@/components/ui/button";
 
 const HeroSection = () => {
 
@@ -11,7 +11,7 @@ const HeroSection = () => {
 
   return (
     <div className={`${styles.banner} container mx-auto border-2 border-white rounded-3xl mt-10 relative`}>
-        <div className="absolute inset-0 rounded-3xl container mx-auto h-[500px] opacity-60 bg-gradient-to-r from-indigo-600 via-black to-purple-400"></div>
+        <div className="absolute inset-0 rounded-3xl container mx-auto h-[500px] opacity-60 bg-gradient-to-r from-indigo-500 via-gray-800 to-purple-400"></div>
 
       <div className="flex flex-col items-center justify-center text-center px-6 py-20 md:py-32">
         <h1 className="text-4xl md:text-6xl font-bold text-white mb-6 drop-shadow-lg">
@@ -21,7 +21,15 @@ const HeroSection = () => {
           Your one-stop destination for affordable and trusted medical products delivered with care.
         </p>
         
-        <CustomButton textName="Shop Now" handleAnything={()=> router.push("/shop")}/>
+        {/* <CustomButton textName="Shop Now" handleAnything={()=> router.push("/shop")}/> */}
+
+        <Button 
+          variant="outline"  
+          className="bg-transparent text-white drop-shadow-lg hover:text-black h-12 z-10"
+          onClick={()=> router.push("/shop")}
+        >
+          Shop Now
+        </Button>
       </div>
     </div>
   );
