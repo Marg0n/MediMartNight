cd2a8592697e0d1b8ce21aabb18c4ded45b18869
MarGoN <monondcosta@gmail.com>
2025-05-06 02:46:19 +0600

    Fix: fixed featured section card list

--- src/components/modules/home/FeaturedSection/FeaturedSection.tsx
+++ src/components/modules/home/FeaturedSection/FeaturedSection.tsx
@@ -44,7 +44,7 @@ const FeaturedSection = () => {
           <Loading />
         </div>
       ) : (
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-6 pt-3">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 pt-3">
           {products
             ?.sort((a: any, b: any) => {
               // First, prioritize in-stock items
